fa36018a8300400f707a1e7ac53d184685379da4
saikrn112 <Sai.Pinnamaraju@Honeywell.com>
2017-07-03 06:18:54 +0530

    completed the processMatrix processMatrixJacobian

--- src/sensor_fusion/src/filter_core.cpp
+++ src/sensor_fusion/src/filter_core.cpp
@@ -17,6 +17,12 @@ namespace FilterCore {
   void EkfCore::predict(const double delta){
     /* Temporary Variables */
     // Quaternion components
+    double vx    = state_(StateVelocityX);
+    double vy    = state_(StateVelocityY);
+    double vz    = state_(StateVelocityZ);
+    double ax    = state_(StateAcclerationX);
+    double ay    = state_(StateAcclerationY);
+    double az    = state_(StateAcclerationZ);
     double q0    = state_(StateQuaternion0);
     double q1    = state_(StateQuaternion1);
     double q2    = state_(StateQuaternion2);
@@ -36,8 +42,8 @@ namespace FilterCore {
     double r21 = 2*(q1*q2 + q3*q0); // Y-X velocity component
     double r13 = 2*(q1*q3 + q2*q0); // X-Z velocity component
     double r31 = 2*(q1*q3 - q2*q0); // Z-X velocity component
-    double r32 = 2*(q2*q3 + q1*q0); // Z-Y velocity component
     double r23 = 2*(q2*q3 - q1*q0); // Y-Z velocity component
+    double r32 = 2*(q2*q3 + q1*q0); // Z-Y velocity component
 
     // Quaternion based calculations :[Refer Price borough's implementation]
     double dax = wx*delta - dax_b;
@@ -46,8 +52,6 @@ namespace FilterCore {
     double daxHalf = dax/2;
     double dayHalf = day/2;
     double dazHalf = daz/2;
-
-
 
     processMatrix_.eye(STATE_SIZE,STATE_SIZE); // @TODO: send it to Constructor
 
@@ -76,7 +80,7 @@ namespace FilterCore {
     processMatrix_(StatePositionZ,StateAcclerationZ) = 0.5*processMatrix_(StatePositionZ,StateVelocityZ)*delta;
     processMatrix_(StateVelocityX,StateAcclerationX) = delta;
     processMatrix_(StateVelocityY,StateAcclerationY) = delta;
-    processMatrix_(StateVelocityZ,StateAcclerationz) = delta;
+    processMatrix_(StateVelocityZ,StateAcclerationZ) = delta;
     processMatrix_(StateQuaternion0,StateQuaternion1) = -daxHalf;
     processMatrix_(StateQuaternion0,StateQuaternion2) = -dayHalf;
     processMatrix_(StateQuaternion0,StateQuaternion3) = -dazHalf;
@@ -90,17 +94,76 @@ namespace FilterCore {
     processMatrix_(StateQuaternion3,StateQuaternion1) =  dayHalf;
     processMatrix_(StateQuaternion3,StateQuaternion2) = -daxHalf;
 
-    // Much of the transfer function Jacocian is identical to the process Matrix
-    // @NOTE pain max; most of it is copied from priceborough who has lot of patience to write all the equations
-    // @NOTE we should give him _/\_
-    // @NOTE need to still write the quaternion based Jacobian :(
+    // Much of the processMatrixJacocian is identical to the processMatrix
     processMatrixJacobian_ = processMatrix_;
-    
+
+    double xCoeff = 2*(vx*delta + 0.5*delta*delta*ax);
+    double yCoeff = 2*(vy*delta + 0.5*delta*delta*ay);
+    double zCoeff = 2*(vz*delta + 0.5*delta*delta*az);
+
+    processMatrixJacobian_(StatePositionX,StateQuaternion0) =  q0*xCoeff - q3*yCoeff + q2*zCoeff;
+    processMatrixJacobian_(StatePositionX,StateQuaternion1) =  q1*xCoeff + q2*yCoeff + q3*zCoeff;
+    processMatrixJacobian_(StatePositionX,StateQuaternion2) = -q2*xCoeff + q1*yCoeff + q0*zCoeff;
+    processMatrixJacobian_(StatePositionX,StateQuaternion3) = -q3*xCoeff - q0*yCoeff + q1*zCoeff;
+    processMatrixJacobian_(StatePositionY,StateQuaternion0) =  q3*xCoeff + q0*yCoeff - q1*zCoeff;
+    processMatrixJacobian_(StatePositionY,StateQuaternion1) =  q2*xCoeff - q1*yCoeff - q0*zCoeff;
+    processMatrixJacobian_(StatePositionY,StateQuaternion2) =  q1*xCoeff + q2*yCoeff + q3*zCoeff;
+    processMatrixJacobian_(StatePositionY,StateQuaternion3) =  q0*xCoeff - q3*yCoeff + q2*zCoeff;
+    processMatrixJacobian_(StatePositionZ,StateQuaternion0) = -q2*xCoeff + q1*yCoeff + q0*zCoeff;
+    processMatrixJacobian_(StatePositionZ,StateQuaternion1) =  q3*xCoeff + q0*yCoeff - q1*zCoeff;
+    processMatrixJacobian_(StatePositionZ,StateQuaternion2) = -q0*xCoeff + q3*yCoeff - q2*zCoeff;
+    processMatrixJacobian_(StatePositionZ,StateQuaternion3) =  q1*xCoeff + q2*yCoeff + q3*zCoeff;
+    processMatrixJacobian_(StateQuaternion0,StateQuaternion1) = -daxHalf;
+    processMatrixJacobian_(StateQuaternion0,StateQuaternion2) = -dayHalf;
+    processMatrixJacobian_(StateQuaternion0,StateQuaternion3) = -dazHalf;
+    processMatrixJacobian_(StateQuaternion1,StateQuaternion0) =  daxHalf;
+    processMatrixJacobian_(StateQuaternion1,StateQuaternion2) =  dazHalf;
+    processMatrixJacobian_(StateQuaternion1,StateQuaternion3) = -dayHalf;
+    processMatrixJacobian_(StateQuaternion2,StateQuaternion0) =  dayHalf;
+    processMatrixJacobian_(StateQuaternion2,StateQuaternion1) = -dazHalf;
+    processMatrixJacobian_(StateQuaternion2,StateQuaternion3) =  daxHalf;
+    processMatrixJacobian_(StateQuaternion3,StateQuaternion0) =  dazHalf;
+    processMatrixJacobian_(StateQuaternion3,StateQuaternion1) =  dayHalf;
+    processMatrixJacobian_(StateQuaternion3,StateQuaternion2) = -dazHalf;
+    processMatrixJacobian_(StateQuaternion0,StateOmegaX) = -q1*delta/2;
+    processMatrixJacobian_(StateQuaternion0,StateOmegaY) = -q2*delta/2;
+    processMatrixJacobian_(StateQuaternion0,StateOmegaZ) = -q3*delta/2;
+    processMatrixJacobian_(StateQuaternion1,StateOmegaX) =  q0*delta/2;
+    processMatrixJacobian_(StateQuaternion1,StateOmegaY) = -q3*delta/2;
+    processMatrixJacobian_(StateQuaternion1,StateOmegaZ) =  q2*delta/2;
+    processMatrixJacobian_(StateQuaternion2,StateOmegaX) =  q3*delta/2;
+    processMatrixJacobian_(StateQuaternion2,StateOmegaY) =  q0*delta/2;
+    processMatrixJacobian_(StateQuaternion2,StateOmegaZ) = -q1*delta/2;
+    processMatrixJacobian_(StateQuaternion3,StateOmegaX) = -q2*delta/2;
+    processMatrixJacobian_(StateQuaternion3,StateOmegaY) =  q1*delta/2;
+    processMatrixJacobian_(StateQuaternion3,StateOmegaZ) =  q0*delta/2;
+    processMatrixJacobian_(StateQuaternion0,StateDeltaAngleBiasX) =  q1/2;
+    processMatrixJacobian_(StateQuaternion0,StateDeltaAngleBiasY) =  q2/2;
+    processMatrixJacobian_(StateQuaternion0,StateDeltaAngleBiasZ) =  q3/2;
+    processMatrixJacobian_(StateQuaternion1,StateDeltaAngleBiasX) = -q0/2;
+    processMatrixJacobian_(StateQuaternion1,StateDeltaAngleBiasY) =  q3/2;
+    processMatrixJacobian_(StateQuaternion1,StateDeltaAngleBiasZ) = -q2/2;
+    processMatrixJacobian_(StateQuaternion2,StateDeltaAngleBiasX) = -q3/2;
+    processMatrixJacobian_(StateQuaternion2,StateDeltaAngleBiasY) = -q0/2;
+    processMatrixJacobian_(StateQuaternion2,StateDeltaAngleBiasZ) =  q1/2;
+    processMatrixJacobian_(StateQuaternion3,StateDeltaAngleBiasX) =  q2/2;
+    processMatrixJacobian_(StateQuaternion3,StateDeltaAngleBiasY) = -q1/2;
+    processMatrixJacobian_(StateQuaternion3,StateDeltaAngleBiasZ) = -q0/2;
 
 
-  }
+
+    // projecting state forward till lastFilterTime+delta   @TODO need to incorporate lastFilterTime parameter
+    state_ = processMatrix_*state_;
+
+    // Project the process noise covariance forward @TODO need to initialise estimateErrorCovariance_ and processNoiseCovariance_
+    // @NOTE need to understand why we need delta in front of processNoiseCovariance_
+    // @TODO need to think about pointers (big matrices) atleast for processNoiseCovariance_
+    estimateErrorCovariance_ = processMatrixJacobian_*estimateErrorCovariance_*processMatrixJacobian_.t() + delta*processNoiseCovariance_;
+
+  } // method EkfCore::process
 
   void EkfCore::update(const sensorMeasurements& measurement){
 
-  }
-}
+  } // method EkfCore::update
+
+}// namespace FilterCore
