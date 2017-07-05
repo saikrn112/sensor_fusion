a3cbe4f5d75c8ca9146cda261de09d9da69bd5a6
saikrn112 <Sai.Pinnamaraju@Honeywell.com>
2017-07-05 18:54:45 +0530

    IMU debug successfull

--- src/sensor_fusion/src/fusion.cpp
+++ src/sensor_fusion/src/fusion.cpp
@@ -8,54 +8,63 @@ namespace Fusion{
 
     void Ekf::imu_cb(const sensor_msgs::Imu::ConstPtr& msg){
       ROS_INFO("in Imu call back");
-      // FilterCore::SensorMeasurementPtr measurementPtr = FilterCore::SensorMeasurementPtr(new FilterCore::SensorMeasurement);
       // //@TODO convert 3x3 covariance to 4x4 covariance. Temporarily filling it with identity
       // //@TODO confirm that all the covariance matrices are just diagonal matrix;
       // //@TODO if they are just constants why not just hard code or parameterise them instead of reading from msgs;
-      // arma::mat anglesCovariance(ANGLES_SIZE,ANGLES_SIZE);
-      // anglesCovariance.eye();
-      // anglesCovariance(0,0) = msg->orientation_covariance[0];
-      // anglesCovariance(1,1) = msg->orientation_covariance[4];
-      // anglesCovariance(2,2) = msg->orientation_covariance[8];
-      // arma::mat quaternionCovariance(QUAT_SIZE,QUAT_SIZE);
-      // quaternionCovariance.eye();
-      // arma::mat accelerationCovariance(ACCELERATION_SIZE,ACCELERATION_SIZE);
-      // accelerationCovariance.eye();
-      // accelerationCovariance(0,0) = msg->linear_acceleration_covariance[0];
-      // accelerationCovariance(1,1) = msg->linear_acceleration_covariance[4];
-      // accelerationCovariance(2,2) = msg->linear_acceleration_covariance[8];
-      // arma::mat omegaCovariance(OMEGA_SIZE,OMEGA_SIZE);
-      // omegaCovariance.eye();
-      // omegaCovariance(0,0) = msg->angular_velocity_covariance[0];
-      // omegaCovariance(1,1) = msg->angular_velocity_covariance[4];
-      // omegaCovariance(2,2) = msg->angular_velocity_covariance[8];
-      //
-      // arma::colvec measurement(STATE_SIZE);
-      // measurement.zeros();
-      // // measurement->measurement_
-      // measurement(StateQuaternion0) = msg->orientation.x; // @TODO make sure that q0 is scalar
-      // measurement(StateQuaternion1) = msg->orientation.y;
-      // measurement(StateQuaternion2) = msg->orientation.z;
-      // measurement(StateQuaternion3) = msg->orientation.w;
-      // measurement(StateAcclerationX) = msg->linear_acceleration.x;
-      // measurement(StateAcclerationY) = msg->linear_acceleration.y;
-      // measurement(StateAcclerationZ) = msg->linear_acceleration.z;
-      // measurement(StateOmegaX) = msg->angular_velocity.x;
-      // measurement(StateOmegaY) = msg->angular_velocity.y;
-      // measurement(StateOmegaZ) = msg->angular_velocity.z;
-      //
-      // arma::mat covariance(STATE_SIZE,STATE_SIZE);
-      // covariance.submat(StateQuaternion0,StateQuaternion0,StateQuaternion3,StateQuaternion3) = quaternionCovariance;
-      // covariance.submat(StateAcclerationX,StateAcclerationX,StateAcclerationZ,StateAcclerationZ) = accelerationCovariance;
-      // covariance.submat(StateOmegaX,StateOmegaX,StateOmegaZ,StateOmegaZ) = omegaCovariance;
-      //
+      arma::mat anglesCovariance(ANGLES_SIZE,ANGLES_SIZE);
+      anglesCovariance.eye();
+      anglesCovariance(0,0) = msg->orientation_covariance[0];
+      anglesCovariance(1,1) = msg->orientation_covariance[4];
+      anglesCovariance(2,2) = msg->orientation_covariance[8];
+
+
+      arma::mat quaternionCovariance(QUAT_SIZE,QUAT_SIZE);
+      quaternionCovariance.eye();
+
+      arma::mat accelerationCovariance(ACCELERATION_SIZE,ACCELERATION_SIZE);
+      accelerationCovariance.eye();
+      accelerationCovariance(0,0) = msg->linear_acceleration_covariance[0];
+      accelerationCovariance(1,1) = msg->linear_acceleration_covariance[4];
+      accelerationCovariance(2,2) = msg->linear_acceleration_covariance[8];
+
+      arma::mat omegaCovariance(OMEGA_SIZE,OMEGA_SIZE);
+      omegaCovariance.eye();
+      omegaCovariance(0,0) = msg->angular_velocity_covariance[0];
+      omegaCovariance(1,1) = msg->angular_velocity_covariance[4];
+      omegaCovariance(2,2) = msg->angular_velocity_covariance[8];
+
+      arma::colvec measurement(STATE_SIZE);
+      measurement.zeros();
+      measurement(StateQuaternion0) = msg->orientation.x; // @TODO make sure that q0 is scalar
+      measurement(StateQuaternion1) = msg->orientation.y;
+      measurement(StateQuaternion2) = msg->orientation.z;
+      measurement(StateQuaternion3) = msg->orientation.w;
+      measurement(StateAcclerationX) = msg->linear_acceleration.x;
+      measurement(StateAcclerationY) = msg->linear_acceleration.y;
+      measurement(StateAcclerationZ) = msg->linear_acceleration.z;
+      measurement(StateOmegaX) = msg->angular_velocity.x;
+      measurement(StateOmegaY) = msg->angular_velocity.y;
+      measurement(StateOmegaZ) = msg->angular_velocity.z;
+
+      arma::mat covariance(STATE_SIZE,STATE_SIZE);
+      covariance.eye();
+
+      covariance.submat(StateQuaternion0,StateQuaternion0,StateQuaternion3,StateQuaternion3) = quaternionCovariance;
+      covariance.submat(StateAcclerationX,StateAcclerationX,StateAcclerationZ,StateAcclerationZ) = accelerationCovariance;
+      covariance.submat(StateOmegaX,StateOmegaX,StateOmegaZ,StateOmegaZ) = omegaCovariance;
+
+
       // //Enqueuing the IMU measurement in the priority queue.
-      // measurementPtr->topicName_ = "IMU";
-      // measurementPtr->measurement_ = measurement;
-      // measurementPtr->covariance_ = covariance;
-      // if(isDebugMode_){
-      //   measurementPtr->measurement_.print();
-      // }
+      FilterCore::SensorMeasurementPtr measurementPtr = FilterCore::SensorMeasurementPtr(new FilterCore::SensorMeasurement);
+      measurementPtr->topicName_ = "IMU";
+      measurementPtr->measurement_ = measurement;
+      measurementPtr->covariance_ = covariance;
+      measurementPtr->time_ = msg->header.stamp.toSec();
+      if(isDebugMode_){
+        ROS_INFO_STREAM("measurement_topic: " << measurementPtr->topicName_ << endl
+                    <<  "measurements: " << endl<< measurementPtr->measurement_ << endl
+                    <<  "covariances: " << endl << measurementPtr->covariance_ << endl);
+      }
     }
 
     void Ekf::gps_cb(const sensor_msgs::NavSatFix::ConstPtr& msg){
