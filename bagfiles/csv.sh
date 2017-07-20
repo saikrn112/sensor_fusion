5358f2b90dc60551b7f46b7431363d621cf91546
saikrn112 <Sai.Pinnamaraju@Honeywell.com>
2017-07-20 20:20:16 +0530

    added new bag file

--- bagfiles/csv.sh
+++ bagfiles/csv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export f=gpsrlodom.bag
+export f=circlerlfusion.bag
 # export topic=/odometry/filtered_map
 export topic=/odomCombined
+export topic1=/odometry/filtered_map
 rostopic echo -p -b $f $topic >${f%.bag}_${topic//\//_}.csv
